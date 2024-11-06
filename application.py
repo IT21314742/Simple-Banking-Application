b2d42abbe00d65680b7e3d715b7fb55caf5437ea
IT21314742 <it21314742@my.sliit.lk>
2024-11-06 08:18:53 +0530

    Update
    Update

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ def show_balance():
     print(f"Your balance is ${balance:.2f}")
 
 def deposit():
-    amount = input("Enter an amount to be de")
+    amount = input("Enter an amount to be dep")
 
 def withdraw():
     pass
