f2f42958ef498fb4b8c76aa015f50ee2784077e2
IT21314742 <it21314742@my.sliit.lk>
2024-11-12 21:16:47 +0530

    Update
    Update

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ def withdraw():
     if amount > balance:
         print("insufficient funds")
     elif amount < 0:
-        print("Amount must be greater than")
+        print("Amount must be greater than 0")
 
 balance = 0
 is_running = True
